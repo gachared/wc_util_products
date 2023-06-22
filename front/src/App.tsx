b4eb151c9b494ee6ec2652febae7b5d5b34647ac
gachared <38619037+gachared@users.noreply.github.com>
2023-06-22 10:50:55 +0900

    style調整

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react';
 import SkuInput from './components/SkuInput';
-import { Container } from '@mui/material';
+import { Container, Divider } from '@mui/material';
 
 const App: FC = () => {
   return (
     <Container>
+      <h2>おすすめ・ランキング設定</h2>
+      <span>英数字または「-」のみ入力可能です</span>
       <SkuInput codeType='おすすめ' codeKey='recommend' />
+      <Divider sx={{ marginY: 3 }} />
       <SkuInput codeType='ランキング' codeKey='ranking' />
     </Container>
   );
